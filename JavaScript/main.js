e2b71b8164b597da6db166febe40a0a293b51943
Robson7 <ismaelerosalia@gmail.com>
2021-09-26 17:03:38 -0300

    Int-JavaScript

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+//alert("Meu primeiro js");
+var nome = "Ismael Robson";
+var idade = 36
+var frase = "Só Jesus Salva"
+//alert("Bem Vindo" + nome);
+//alert("Você tem" + idade + "anos")
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Salva", "Cura").toUpperCase());
+alert(frase.toUpperCase());
